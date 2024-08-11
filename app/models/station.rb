a0a6b5cb13b7699bb45ebf81a2e578f29004b807
Bruno Sacco <brusacco@gmail.com>
2024-08-10 20:34:16 -0400

    Update Station model to include stream_url attribute

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,7 +5,7 @@ class Station < ApplicationRecord
   has_many :videos, dependent: :destroy
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[created_at directory id id_value name updated_at]
+    %w[created_at directory id id_value name updated_at stream_url]
   end
 
   def self.ransackable_associations(_auth_object = nil)
