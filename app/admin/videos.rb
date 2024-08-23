bd351c0a6e613224063579cadb30b81fe6413c03
Fingolfin <v.pedromartinez@gmail.com>
2024-08-23 09:26:49 -0400

    Add video preview on admin

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -14,6 +14,13 @@ ActiveAdmin.register Video do
     column :posted_at
     column :station
     column :transcription
+
+    # column 'Video' do |video|
+    #   video(width: 320, height: 240, controls: true) do
+    #     source(src: video.public_path, type: 'video/mp4')
+    #   end
+    # end
+
     actions
   end
 
@@ -26,4 +33,26 @@ ActiveAdmin.register Video do
     end
     actions
   end
+
+  show do |video|
+    attributes_table do
+      row :location
+      row 'Fecha', &:posted_at
+      row 'Estacion', &:station
+      row :path
+      row :thumbnail
+      row :public_path
+      row :transcription
+      row :created_at
+      row :updated_at
+  
+      row :video do |file| 
+        video(width: 480, height: 320, controls: true, autobuffer: true) do
+          source(src: file.public_path, type: 'video/mp4')
+        end
+        # video_tag url_for('https://www.youtube.com/watch?v=q1gnOM88OjU'), type: "video/mp4", controls: true, size: '480x320'
+      end
+
+    end
+  end   
 end
