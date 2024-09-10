fb9c9e68fd95a8a422d1134fdf7ef0efa2d59a0d
Bruno Sacco <brusacco@gmail.com>
2024-09-10 14:51:03 -0400

    Refactor admin/stations.rb to add filters and columns in the index view

--- app/admin/stations.rb
+++ app/admin/stations.rb
@@ -2,4 +2,18 @@
 
 ActiveAdmin.register Station do
   permit_params :name, :directory, :stream_url, :stream_status
+
+  filter :name
+  filter :stream_status, as: :select
+
+  index do
+    column :id
+    column :name
+    column :directory
+    column :stream_status do |station|
+      status_tag(station.stream_status)
+    end
+    column :created_at
+    column :updated_at
+  end
 end
