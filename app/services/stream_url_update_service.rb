ab0ffc00e2a576484ea3868055c29cfdc7ff43be
Bruno Sacco <brusacco@gmail.com>
2025-11-18 15:48:37 -0300

    aaa

--- app/services/stream_url_update_service.rb
+++ app/services/stream_url_update_service.rb
@@ -171,7 +171,7 @@ class StreamUrlUpdateService < ApplicationService
     options.add_argument("--user-agent=#{USER_AGENT}")
     options.add_argument("--proxy-server=http://#{proxy_ip}")
     options.add_argument('--timeout=60')
-    options.add_argument("--user-data-dir=#{@user_data_dir}")
+    # options.add_argument("--user-data-dir=#{@user_data_dir}")
     options.add_argument('--disable-features=ChromeUserDataDirLocking')
 
     driver = Selenium::WebDriver.for(:chrome, options: options)
